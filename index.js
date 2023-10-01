7cd8e6ac33a80fb2aacce915f57f62bfe8557289
muhammedkurtulus <muhammedkurtulus06@gmail.com>
2023-10-01 15:42:01 +0300

    added cors

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const user_model = require("./user-model");
 
 app.use(express.json());
 app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
   next();
 });
 
